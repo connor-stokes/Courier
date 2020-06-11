59831b90c0d0e088c9e6f5ca94ab7c7596f4286d
connor-stokes <connor.stokes@ymail.com>
2020-06-11 07:45:18 +0100

    removing redundant test

--- __tests__/controllers/CourierController.test.ts
+++ __tests__/controllers/CourierController.test.ts
@@ -3,13 +3,7 @@ import CourierController from '../../src/controllers/couriers.controller'
 
 describe('The CourierController;', () => {
   const result = new CourierController()
-  describe('', () => {
-
-  })
   it('It can be called;', () => {
     expect(result).toBeTruthy()
   })
-  // it('Index returns as expected;', () => {
-  //   expect(result.index).toBe('function')
-  // })
 })
